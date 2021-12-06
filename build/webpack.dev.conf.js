0e3b2e1b7af27d5809d9885ba0527f17fb96782d
a602988 <a680603@gmail.com>
2021-12-06 13:26:24 +0800

    ok

--- build/webpack.dev.conf.js
+++ build/webpack.dev.conf.js
@@ -5,9 +5,6 @@ const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 module.exports = merge(common, {
     mode: 'development',
     devtool: 'inline-source-map',
-    devServer: {
-        static: './dist',
-    },
     plugins: [
         //將css獨立出來
         new MiniCssExtractPlugin({
