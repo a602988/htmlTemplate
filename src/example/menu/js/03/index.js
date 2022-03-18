d020b88cb1730a568dfbaa17e06f5dd5ea5c4c09
a602988 <a680603@gmail.com>
2022-03-18 16:26:51 +0800

    1
    1

--- src/example/menu/js/03/index.js
+++ src/example/menu/js/03/index.js
@@ -5,17 +5,38 @@ import anime from 'animejs/lib/anime.es.js';
 
 $(function() {
 
-  // "use strict";
-  // var $html   = document.querySelector("html");
-  // var $navbar_btn = document.querySelector(".navbar-toggler");
-  // var $navbar = document.querySelector(".navbar");
+  "use strict";
+
+  //let elem = document.querySelector('.test1').getBoundingClientRect();
+
+  const nav = document.querySelector('.header-nav-main > nav > .nav .dropdown-submenu');
+  const sub = nav.getElementsByTagName("dropdown-menu");
   //
+  // var testElements = document.getElementsByClassName('.header-nav-main > nav > .nav .dropdown-submenu');
+  // var testDivs = Array.prototype.filter.call(testElements, function(testElement){
   //
-  //
-  // $navbar_btn.addEventListener("click", function () {
-  //   $html.classList.toggle('header-active');
-  // }, false);
+  //   return testElement.nodeName === 'ul';
+  // });
+  // console.log(testDivs);
 
 
+  var element= document.getElementsByClassName('dropdown-submenu');
+  for(var i=0;i<element.length;i++){
+    console.log(element.length);
+  }
 
+  // if(nav.nodeName = "dropdown-menu"){
+  //   console.log(1);
+  // }
+  // console.log(sub.getBoundingClientRect);
+  // nav.addEventListener('mouseover',function(e){
+  //
+  //
+  //   if (sub){
+  //     const subData = sub.clientWidth;
+  //     console.log(sub);
+  //   }
+  //   //console.log(sub.childNodes);
+  //  //console.log(nav.getBoundingClientRect());
+  // },false)
 });
