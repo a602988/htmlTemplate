d339d9fe766d528a63123ecd5aeaeb878be02e95
a602988 <a680603@gmail.com>
2022-03-22 16:15:46 +0800

    1
    1

--- src/example/menu/js/03/index.js
+++ src/example/menu/js/03/index.js
@@ -9,34 +9,56 @@ $(function() {
 
 
   /*
+  選單左右多層次：li.dropdown.dropdown-x
   1. 判斷nav的第一層每一個li的x左標
   2. 取得li 下的 dropdown-menu 寬度
   3. 如果寬度+ x座標，大於螢幕寬度
-  4. 則所有子層.dropdown-menu 增加 .dropdown-menu-right
+  4. 則子層.dropdown-menu 增加 .dropdown-menu-right
+  */
+
+  // var windowWidth = window.innerWidth;
+  // const elt = document.querySelectorAll('.header-nav-main li.dropdown-x');
+  // for (let i = 0; i < elt.length; i++) {
+  //   let val = elt[i].getBoundingClientRect();
+  //   let sub = elt[i].querySelector('.dropdown-menu');
+  //   let subW = sub.getBoundingClientRect();
+  //   let mW = val.x + subW.width;
+  //   console.log(mW,windowWidth);
+  //   if (mW > windowWidth) {
+  //     sub.classList.add("dropdown-menu-right");
+  //   }
+  // }
+
+
+  //console.log(totalW,windowWidth);
+  /*
+  選單左右多層次：li.dropdown.dropdown-x
+  1. 判斷nav的第一層每一個li的x左標
+  2. 取得li 下的 dropdown-menu 所有寬度
+  3. 如果所有寬度+ x座標，大於螢幕寬度
+  4. 則子層.dropdown-menu 增加 .dropdown-menu-right
   */
 
   var windowWidth = window.innerWidth;
-  var windowHeight = window.innerHeight;
 
-  console.log(windowWidth,windowHeight);
+  const elt = document.querySelectorAll('.header-nav-main > nav > ul > li.dropdown-x');
 
-  const elt = document.querySelectorAll('.header-nav-main li.dropdown-x');//次選單為左右展開的
-  const submenuW = 200;//子選單寬度
-  console.log("elt.length:" + elt.length);
   for (let i = 0; i < elt.length; i++) {
-    let val = elt[i].getBoundingClientRect();
-    let sub = elt[i].querySelectorAll('dropdown-menu');
-    //console.log(val);
-    console.log(sub);
-    let mW = val.x + submenuW;
-    console.log("mW:" + mW);
-    if ( mW > windowWidth){
-      console.log(sub.length);
-      for (let i = 0; i < sub.length; i++) {
-        let o = sub[i];
-        console.log(sub.length);
-        o.setAttribute("class","dropdown-menu-right");
-      }
+    let nav = elt[i];//取得第一層選單值
+    let navVal = nav.getBoundingClientRect();//取得第一層選單值
+
+    let nav2Ul = nav.querySelector('.dropdown-menu');//取得第二層內選單
+    let nav2UlVal = nav2Ul.getBoundingClientRect();//取得第二層內選單 值
+
+    let nav3 = nav2Ul.getElementsByClassName('dropdown-menu');//取得第三層內選單
+
+
+
+    let totalW = navVal.x + nav2UlVal.width;// 第一層x位置 + 第二層選單寬度
+
+
+    if (totalW > windowWidth) {
+      nav2Ul.classList.add("dropdown-menu-right");
     }
   }
 });
