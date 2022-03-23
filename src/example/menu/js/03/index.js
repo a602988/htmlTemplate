7fbfb3d8e58ca8a889f3a70f48950ed68045abb7
a602988 <a680603@gmail.com>
2022-03-23 17:25:56 +0800

    nav
    nav

--- src/example/menu/js/03/index.js
+++ src/example/menu/js/03/index.js
@@ -1,64 +1,83 @@
 //載入jquery
 import $ from "jquery";
 import './style.scss';
-import anime from 'animejs/lib/anime.es.js';
+import Class from 'src/assets/js/components/class.js'
 
 $(function() {
 
   "use strict";
 
-
-  /*
-  選單左右多層次：li.dropdown.dropdown-x
-  1. 判斷nav的第一層每一個li的x左標
-  2. 取得li 下的 dropdown-menu 寬度
-  3. 如果寬度+ x座標，大於螢幕寬度
-  4. 則子層.dropdown-menu 增加 .dropdown-menu-right
-  */
-
-  // var windowWidth = window.innerWidth;
-  // const elt = document.querySelectorAll('.header-nav-main li.dropdown-x');
-  // for (let i = 0; i < elt.length; i++) {
-  //   let val = elt[i].getBoundingClientRect();
-  //   let sub = elt[i].querySelector('.dropdown-menu');
-  //   let subW = sub.getBoundingClientRect();
-  //   let mW = val.x + subW.width;
-  //   console.log(mW,windowWidth);
-  //   if (mW > windowWidth) {
-  //     sub.classList.add("dropdown-menu-right");
-  //   }
-  // }
-
-
   //console.log(totalW,windowWidth);
   /*
   選單左右多層次：li.dropdown.dropdown-x
+  適用：大於1440的螢幕，不要超過三層，會有響應式超出問題
   1. 判斷nav的第一層每一個li的x左標
   2. 取得li 下的 dropdown-menu 所有寬度
-  3. 如果所有寬度+ x座標，大於螢幕寬度
+  3. 如果2的寬度+ x座標，大於螢幕寬度
   4. 則子層.dropdown-menu 增加 .dropdown-menu-right
+  5. 如果有第三層，底下子層 .dropdown-menu 增加 .dropdown-menu-right
   */
 
-  var windowWidth = window.innerWidth;
+  let windowWidth = window.innerWidth;
 
   const elt = document.querySelectorAll('.header-nav-main > nav > ul > li.dropdown-x');
+  const eltNum = elt.length;
 
-  for (let i = 0; i < elt.length; i++) {
-    let nav = elt[i];//取得第一層選單值
+
+  for (let i = 0; i < eltNum; i++) {
+    const nav = elt[i];//取得第一層選單值
     let navVal = nav.getBoundingClientRect();//取得第一層選單值
 
-    let nav2Ul = nav.querySelector('.dropdown-menu');//取得第二層內選單
+    const nav2Ul = nav.querySelector('.dropdown-menu');//取得第二層內選單
     let nav2UlVal = nav2Ul.getBoundingClientRect();//取得第二層內選單 值
-
-    let nav3 = nav2Ul.getElementsByClassName('dropdown-menu');//取得第三層內選單
-
 
 
     let totalW = navVal.x + nav2UlVal.width;// 第一層x位置 + 第二層選單寬度
+    let nav3 = nav2Ul.getElementsByClassName('dropdown-menu');//取得第三層內選單
+    const nav3Num = nav3.length;
 
+    if (nav3Num > 0){
+      let mixW = 0;
+      let totalW2 = 0;// 第一層x位置 + 第二層選單寬度
+      for (let i = 0; i < nav3Num; i++) {
+        let nav3UlVal = nav3[i].getBoundingClientRect();//取得第三層內選單 值
+        if (nav3UlVal.width > mixW){
+          mixW = nav3UlVal.width;
+        }
+        totalW2 = navVal.x + nav2UlVal.width + mixW;// 第一層x位置 + 第二層選單寬度
+      }
+      console.log(navVal.x , nav2UlVal.width,mixW,totalW2);
+      if (totalW2 > windowWidth) {
+        Class.addClass(nav2Ul, 'dropdown-menu-right')
+        if (nav3Num > 0){
+          for (let i = 0; i < nav3Num; i++) {
+            Class.addClass(nav3[i], 'dropdown-menu-right')
+          }
+        }
+      }
+    }
 
     if (totalW > windowWidth) {
-      nav2Ul.classList.add("dropdown-menu-right");
+      Class.addClass(nav2Ul, 'dropdown-menu-right')
+      if (nav3Num > 0){
+        for (let i = 0; i < nav3Num; i++) {
+          Class.addClass(nav3[i], 'dropdown-menu-right')
+        }
+      }
     }
+
   }
+  var navx = (o) => {
+    let elt = o,
+      eltNum  = o.length,
+      price = 0,
+      discount = 0,
+      fullPrice  = 0,
+      myArray = [],
+      myObject = {};
+    console.log(o)
+  }
+  navx(elt);
+
+
 });
